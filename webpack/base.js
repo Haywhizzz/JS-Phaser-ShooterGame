3e6953195ff0240b6eb75a494fcc4fca04f63b3b
haywhizzz <haywhizzz@gmail.com>
2021-03-15 21:09:24 +0100

    Fix linter errors

--- webpack/base.js
+++ webpack/base.js
@@ -1,9 +1,7 @@
 const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const {
-  CleanWebpackPlugin,
-} = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
